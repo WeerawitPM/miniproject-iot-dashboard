5bef3def51270fdd00ca669fd2064e47ce1d0d92
WeerawitPM <63030079@kmitl.ac.th>
2022-10-28 11:05:09 +0700

    update chart

--- src/pages/dashboard/realtime/realtimechart.js
+++ src/pages/dashboard/realtime/realtimechart.js
@@ -13,46 +13,29 @@ import axios from "axios";
 
 const data = [
     {
-        name: "Page A",
-        uv: 4000,
-        pv: 2400,
-        amt: 2400
+        Day: "Monday",
+        Temperature_C: 25,
+        Humadity: 80,
     },
     {
-        name: "Page B",
-        uv: 3000,
-        pv: 1398,
-        amt: 2210
+        Day: "Tuesday",
+        Temperature_C: 26,
+        Humadity: 85,
     },
     {
-        name: "Page C",
-        uv: 2000,
-        pv: 9800,
-        amt: 2290
+        Day: "Wednesday",
+        Temperature_C: 27,
+        Humadity: 75,
     },
     {
-        name: "Page D",
-        uv: 2780,
-        pv: 3908,
-        amt: 2000
+        Day: "Thursday",
+        Temperature_C: 28,
+        Humadity: 81,
     },
     {
-        name: "Page E",
-        uv: 1890,
-        pv: 4800,
-        amt: 2181
-    },
-    {
-        name: "Page F",
-        uv: 2390,
-        pv: 3800,
-        amt: 2500
-    },
-    {
-        name: "Page G",
-        uv: 3490,
-        pv: 4300,
-        amt: 2100
+        Day: "Friday",
+        Temperature_C: 29,
+        Humadity: 82,
     }
 ];
 
